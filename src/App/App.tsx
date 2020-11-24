efa9aaa77e176d69629592d8ea2ac15cee217564
VladyslavO <omelchenkovladyslav@gmail.com>
2020-11-24 03:00:08 +0100

    [ feature ]: init robot font

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CreateAccount from "../CreateAccount/CreateAccount";
+
+function App() {
+  return (
+    <div className="App">
+      <CreateAccount/>
+    </div>
+  );
+}
+
+export default App;
